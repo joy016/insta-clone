459faaf59596ee323c8d0b99c571b4389ebeff20
joy016 <jhoylejas@gmail.com>
2023-05-10 20:57:53 +0800

    Initial commit

---
+++ src/app/component/SideMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { AiFillHome } from 'react-icons/ai';
+const SideMenu = () => {
+  return (
+    <>
+      <AiFillHome />
+    </>
+  );
+};
+
+export default SideMenu;
